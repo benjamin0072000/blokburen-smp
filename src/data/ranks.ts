1ed18f2d1202c9aad870b0e7097c9df8ef07a3ad
benjamin0072000 <benjamin0072000@gmail.com>
2026-09-09 00:44:46 +0200

    Voeg speeltijdranks toe

---
+++ src/data/ranks.ts
@@ -0,0 +1,7 @@
+export const ranks = [
+  {name: 'Nieuweling', hours: 0, homes: 1, claims: 2},
+  {name: 'Avonturier', hours: 3, homes: 2, claims: 4},
+  {name: 'Overlever', hours: 12, homes: 3, claims: 8},
+  {name: 'Veteraan', hours: 30, homes: 5, claims: 16},
+  {name: 'Legende', hours: 75, homes: 8, claims: 30},
+];
